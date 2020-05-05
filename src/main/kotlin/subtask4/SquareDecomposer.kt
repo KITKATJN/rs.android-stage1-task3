e34749af72697b7a9303ba1c04a5a47cbeda592d
KITKATJN <lvutinilyha@gmail.com>
2020-05-05 22:46:40 +0300

    add: forth  task

--- src/main/kotlin/subtask4/SquareDecomposer.kt
+++ src/main/kotlin/subtask4/SquareDecomposer.kt
@@ -1,9 +1,35 @@
 package subtask4
 
+import kotlin.math.floor
+import kotlin.math.sqrt
+
 class SquareDecomposer {
 
-    // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        return decomposeValue(number * number, number - 1)
+    }
+
+    private fun decomposeValue(squareOfGivenNumber: Int, prevNumber: Int): Array<Int>? {
+        var penNumber=prevNumber
+        while(  penNumber >=1 ) {
+            val desiredSquare = penNumber * penNumber
+            val currentSquare = squareOfGivenNumber - desiredSquare
+            var sqrt = floor( sqrt(currentSquare.toDouble()) ).toInt()//округление в нижнюю сторону
+            when{
+                currentSquare== 0-> return arrayOf(penNumber)
+                currentSquare < 0-> return null
+                else-> {
+                    if (sqrt >= penNumber) {
+                        sqrt = penNumber - 1
+                    }
+                    val decomposed = decomposeValue(currentSquare, sqrt)//рекурсия
+                    if (null != decomposed) {
+                        return decomposed + arrayOf(penNumber)
+                    }
+                }
+            }
+            penNumber--
+        }
+        return null
     }
 }
