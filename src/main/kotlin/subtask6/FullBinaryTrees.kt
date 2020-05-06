60030dd1af2de0d1dbd97a16fae273a7007b2cf3
KITKATJN <lvutinilyha@gmail.com>
2020-05-06 10:45:46 +0300

    add: no time six  task

--- src/main/kotlin/subtask6/FullBinaryTrees.kt
+++ src/main/kotlin/subtask6/FullBinaryTrees.kt
@@ -4,6 +4,11 @@ class FullBinaryTrees {
 
     // TODO: Complete the following function
     fun stringForNodeCount(count: Int): String {
-        throw NotImplementedError("Not implemented")
+        return when{
+            count%2==0-> "[]"
+            count==0-> "[]"
+            count==1-> "[[0]]"
+            else-> ""
+        }
     }
 }
